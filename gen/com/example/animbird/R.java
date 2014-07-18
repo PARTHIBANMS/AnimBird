791ffc763096bc5e2bf138d509df32a0b35ed9d5
PARTHIBAN M.S <bsauniv30@BSAs-iMac-93.local>
2014-07-18 15:41:58 +0530

    18.7.14

--- gen/com/example/animbird/R.java
+++ gen/com/example/animbird/R.java
@@ -15,8 +15,7 @@ public final class R {
         public static final int abc_slide_in_top=0x7f040003;
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
-        public static final int move=0x7f040006;
-        public static final int sequential=0x7f040007;
+        public static final int flyingbird=0x7f040006;
     }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
@@ -1082,6 +1081,7 @@ containing a value of this type.
         public static final int background=0x7f020057;
         public static final int ic_launcher=0x7f020058;
         public static final int puppy=0x7f020059;
+        public static final int settings=0x7f02005a;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1099,14 +1099,14 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int btnSpeak=0x7f050041;
-        public static final int btnStart=0x7f05003f;
+        public static final int btnSpeak=0x7f050040;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int container=0x7f05003c;
         public static final int default_activity_button=0x7f050028;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
+        public static final int dismiss=0x7f050041;
         public static final int dropdown=0x7f05000f;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
@@ -1117,7 +1117,6 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imgLogo=0x7f05003e;
         public static final int letterView=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
@@ -1125,6 +1124,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int openpopup=0x7f05003e;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1147,7 +1147,7 @@ containing a value of this type.
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
-        public static final int txtText=0x7f050040;
+        public static final int txtText=0x7f05003f;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1203,10 +1203,9 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int activity_move=0x7f030019;
-        public static final int activity_sequential=0x7f03001a;
-        public static final int main=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int main=0x7f030019;
+        public static final int popup=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/example/animbird/MainActivity.java
+++ src/com/example/animbird/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.animbird;
 
+
 import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
@@ -12,18 +14,25 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.view.ViewGroup;
 import android.widget.AbsoluteLayout;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.os.Build;
 
 @SuppressWarnings({ "deprecation", "unused" })
 @SuppressLint("ClickableViewAccessibility")
-public class MainActivity extends Activity implements OnTouchListener{
+public class MainActivity extends Activity implements AnimationListener, OnTouchListener{
 	private ImageView letterView;                       // The letter that the user drags.
 	private ImageView emptyLetterView;              // The letter outline that the user is supposed to drag letterView to.
 	private AbsoluteLayout mainLayout;
+	Animation animSequential;
+	private static final String DEBUG_TAG = "MyActivity";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,15 +41,64 @@ public class MainActivity extends Activity implements OnTouchListener{
 		mainLayout.setOnTouchListener(this);
 		letterView = (ImageView) findViewById(R.id.letterView);
 		letterView.setOnTouchListener(this);
+		letterView = (ImageView) findViewById(R.id.letterView);
+		
+		
+		
+		
+		
+		
+        final Button btnOpenPopup = (Button)findViewById(R.id.openpopup);
+        btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
 
-		//emptyLetterView = (ImageView) findViewById(R.id.emptyLetterView);
-//		if (savedInstanceState == null) {
-//			getFragmentManager().beginTransaction()
-//			.add(R.id.container, new PlaceholderFragment()).commit();
-//		}
-	}
-	private boolean dragging = false;
-	private Rect hitRect = new Rect();
+   @Override
+   public void onClick(View arg0) {
+    LayoutInflater layoutInflater 
+     = (LayoutInflater)getBaseContext()
+      .getSystemService(LAYOUT_INFLATER_SERVICE);  
+    View popupView = layoutInflater.inflate(R.layout.popup, null);  
+             final PopupWindow popupWindow = new PopupWindow(
+               popupView, 
+               LayoutParams.WRAP_CONTENT,  
+                     LayoutParams.WRAP_CONTENT);  
+             
+             Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+             btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+     @Override
+     public void onClick(View v) {
+      // TODO Auto-generated method stub
+      popupWindow.dismiss();
+     }});
+               
+             popupWindow.showAsDropDown(btnOpenPopup, 50, -30);
+         
+   }});
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//		if (savedInstanceState == null) {
+		//			getFragmentManager().beginTransaction()
+		//			.add(R.id.container, new PlaceholderFragment()).commit();
+		//		}
+}
+// private boolean dragging = false;
+//	private Rect hitRect = new Rect();
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -65,19 +123,19 @@ public class MainActivity extends Activity implements OnTouchListener{
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-//	public static class PlaceholderFragment extends Fragment {
-//
-//		public PlaceholderFragment() {
-//		}
-//
-//		@Override
-//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//				Bundle savedInstanceState) {
-//			View rootView = inflater.inflate(R.layout.fragment_main, container,
-//					false);
-//			return rootView;
-//		}
-//	}
+	//	public static class PlaceholderFragment extends Fragment {
+	//
+	//		public PlaceholderFragment() {
+	//		}
+	//
+	//		@Override
+	//		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	//				Bundle savedInstanceState) {
+	//			View rootView = inflater.inflate(R.layout.fragment_main, container,
+	//					false);
+	//			return rootView;
+	//		}
+	//	}
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
@@ -89,15 +147,37 @@ public class MainActivity extends Activity implements OnTouchListener{
 		if (action == MotionEvent.ACTION_DOWN) {
 			if (v == letterView) {
 				//letterView.setImageResource(R.drawable.ic_launcher);
-				dragging = true;
+			//	dragging = true;
 				eventConsumed = false;
 			}
-			
+
 			setAbsoluteLocationCentered(letterView, x, y);
 			
+
+//			// load the animation
+//			animSequential = AnimationUtils.loadAnimation(getApplicationContext(),
+//					R.anim.flyingbird);
+//
+//			// set animation listener
+//			animSequential.setAnimationListener(this);
+//			
+//			letterView.startAnimation(animSequential);
+//			
+//			System.out.println("Animation running");
+//			
+
 		} else if (action == MotionEvent.ACTION_UP) {
 			
-
+		//	
+			
+			
+			
+//		letterView.setVisibility (View.INVISIBLE);			
+//		System.out.println("Animation stopped");
+			
+			
+			
+			
 			//		if (dragging) {
 			//			emptyLetterView.getHitRect(hitRect);
 			//			if (hitRect.contains(x, y)) {
@@ -134,6 +214,27 @@ public class MainActivity extends Activity implements OnTouchListener{
 		alp.x = x;
 		alp.y = y;
 		v.setLayoutParams(alp);
+	}
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		// Take any action after completing the animation
+
+		// check for zoom in animation
+		if (animation == animSequential) {
+		}
+
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation animation) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onAnimationStart(Animation animation) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
